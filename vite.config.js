c8553dae918b3abcb1b0190aea42599cdf29afb4
A6ee <moebby0520@gmail.com>
2026-03-06 11:26:06 +0800

    PWA

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "認知破壞終結者3.0 ✨",
+        short_name: "認知破壞終結者",
+        description: "您的 AI 課堂筆記助手",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
